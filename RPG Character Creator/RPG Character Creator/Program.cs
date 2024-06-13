5b8e62c3ba6292ba557b2709c2e2b9dcaea159f5
PrincessHornage <princesshornage@gmail.com>
2024-06-13 14:20:11 -0400

    Finished RPG Character Creator

---
+++ RPG Character Creator/RPG Character Creator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+/*
+   Princess-Osaani Hornage 2024
+
+   RPG Character Creator - Simulates an RPG adventure with hard-coded attribute stats
+ */
+namespace RPG_Character_Creator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates Character 
+            RPGCharacter player1 = new RPGCharacter("Grimbil Baggins", 17, 9, 14, 21);
+
+            //Lists all player attributes 
+            player1.PrintStats();
+
+            //Player gets attaked
+            player1.GetAttacked();
+
+            //Player reads a book 
+            player1.ReadBook();
+
+            //Player takes a nap 
+            player1.TakeNap();
+
+            //Reprints stats
+            player1.PrintStats();
+
+            //Creates Player 2
+            RPGCharacter player2 = new RPGCharacter("Shiro Baggins", 1, 2, 3, 3);
+
+            player2.PrintStats();
+
+            player2.GoSuperSaiyan();
+
+            player2.PrintStats(); 
+
+        }
+    }
+}
---
+++ RPG Character Creator/RPG Character Creator/RPGCharacter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG_Character_Creator
+{
+    class RPGCharacter
+    {
+        //Fields
+        private string name;
+        private double strength;
+        private double dexterity;
+        private double intelligence;
+        private double health;
+        private double speed; 
+
+        //Constructor
+        public RPGCharacter(string name, int strength, int dexterity, int intelligence, int health)
+        {
+            //Initalizes Fields 
+            this.name = name;
+            this.dexterity = dexterity; 
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.health = health;
+            speed = Math.Floor((health + dexterity)/4.0); //formula to calculate speed 
+        }
+
+        //Decreases Health
+        public void GetAttacked()
+        {
+            Console.WriteLine($"{name} was attacked.");
+            health -= 4;
+            strength++;
+            dexterity++; 
+            Console.WriteLine("\tHealth decreased by 4 points ");
+            Console.WriteLine("\tStrength and dexterity increase by 1 point");
+            speed = Math.Round((health + dexterity) / 4.0);
+            Console.WriteLine();
+        }
+
+        //Increases Intelligence
+        public void ReadBook()
+        {
+            intelligence += 2;
+            Console.WriteLine($"{name} read a book"); 
+            Console.WriteLine("\tIntelligence increases by 2 points");
+            Console.WriteLine();
+
+        }
+
+        //Increases Health 
+        public void TakeNap()
+        {
+            Console.WriteLine($"{name} took a nap");
+            health += 6;
+            Console.WriteLine("\tIncreased health by 6 points");
+            speed = Math.Round((health + dexterity) / 4.0);
+            Console.WriteLine();
+        }
+
+        //Increases Strength 
+        public void GoSuperSaiyan()
+        {
+            Console.Write($"{name} went Super Saiyan");
+            strength += 11;
+            health -= 15;
+            Console.WriteLine("\tStrength increased by 11");
+            Console.WriteLine("\tHealth decreased by 11");
+            speed = Math.Round((health + dexterity) / 4.0);
+            Console.WriteLine();
+        }
+
+        //Prints all player stats
+        public void PrintStats()
+        {
+            Console.WriteLine("RPG Character");
+            Console.WriteLine($"\tName: {name} \n\tStrength: {strength} " +
+                $"\n\tDexterity: {dexterity} \n\tIntelligence: {intelligence}" +
+                $"\n\tHealth: {health} \n\tSpeed: {speed}");
+            Console.WriteLine();
+        }
+    }
+}

caa46d8903a7ed5cce657ac6be5930b0eff17304
PrincessHornage <princesshornage@gmail.com>
2025-01-15 17:55:36 -0500

    - added more content to options screen - added breakpoints for larger screens to handle instructions - added buttons - added conditions to change buttons design on click - minor bug fixes

--- Feeding Fido/src/gameUI.js
+++ Feeding Fido/src/gameUI.js
@@ -2,20 +2,35 @@ const instructionsBtn = document.querySelector("#instructionsBtn");
 const instructionsScreen = document.querySelector("#instructionsScreen");
 const creditsBtn = document.querySelector("#creditsBtn");
 const optionsBtn = document.querySelector("#optionsBtn");
+const optionsScreen = document.querySelector("#optionsScreen"); 
 const startScreen = document.querySelector("#startScreen"); 
-const closeInstBtn = document.querySelector("#closeBtn"); 
 const gameTitle = document.querySelector("#gameTitle"); 
 
+const closeInstBtn = document.querySelector("#closeBtn"); 
+const volumeBtnImg = document.querySelector("#volumeBtnImg"); 
+const volumeBtn = document.querySelector("#volumeBtn"); 
+const efxBtnImg = document.querySelector("#efxBtnImg"); 
+const efxBtn = document.querySelector("#efxBtn"); 
+const saveSettingsBtn = document.querySelector("#saveSettingsBtn"); 
+const fullScreenBtn = document.querySelector("#fullscreenBtn"); 
 
 const gameStartDiv = document.querySelector("#startScreen");
 const gameStartBtn = document.querySelector("#startGameBtn");
 const gameEndDiv = document.querySelector("#gameOverScreen");
 const gameResults = document.querySelector("#gameWinLoseSpan");
-const finalScore = document.querySelector("#gameEndScoreSpan ");
+const finalScore = document.querySelector("#gameEndScoreSpan");
 const gameScreen = document.querySelector("#gameCanvas"); 
 const pauseBtn = document.querySelector("#pauseBtn");
 
-//UI Button Events 
+//efxBtnImg
+
+let volClicks = 0; //Button click count 
+let efxVolClicks = 0; //Button click count 
+let fsClicks = 0; 
+
+//********** UI Button Events ***************
+
+//Displays credits screen 
 creditsBtn.addEventListener("click", ()=> {
 
     //Hide all other screens 
@@ -24,7 +39,6 @@ creditsBtn.addEventListener("click", ()=> {
 
     //Display Main Menu Button 
 });
-
 //Displays game controls and instructions 
 instructionsBtn.addEventListener("click", ()=> {
 
@@ -39,18 +53,14 @@ instructionsBtn.addEventListener("click", ()=> {
     //Display Main Menu Button 
     
 });
-
 //Displays options screen
 optionsBtn.addEventListener("click", ()=> {
 
-    //Hide all other screens 
     startScreen.style.display = "none"; 
-    //Display credits screen 
-
+    optionsScreen.style.display = "block"; 
+    gameTitle.style.display = "none"; 
     
-    //Display Main Menu Button 
 });
-
 //Returns to main menu 
 closeInstBtn.addEventListener("click", ()=> {
 
@@ -60,4 +70,42 @@ closeInstBtn.addEventListener("click", ()=> {
     instructionsScreen.style.display = "none"; 
 
 });
+
+saveSettingsBtn.addEventListener("click", () => {
+    optionsScreen.style.display = "none"; 
+    startScreen.style.display = "flex"; 
+    gameTitle.style.display = "flex"; 
+}); 
+
+
+//Volume On/Off Buttons 
+//Switches image to match audio state 
+volumeBtn.addEventListener("click", ()=> {
+    volClicks++; 
+    if(volClicks === 1){
+        volumeBtnImg.src = "/assets/mute.png"; 
+    } else if( volClicks === 2) {
+        volumeBtnImg.src = "/assets/speaker.png"; 
+        volClicks = 0; 
+    }
+});
+efxBtn.addEventListener("click", ()=> {
+    efxVolClicks++; 
+    if(efxVolClicks === 1){
+        efxBtnImg.src = "/assets/mute.png"; 
+    } else if( efxVolClicks === 2) {
+        efxBtnImg.src = "/assets/speaker.png"; 
+        efxVolClicks = 0; 
+    }
+}); 
+
+fullScreenBtn.addEventListener("click", ()=> {
+    fsClicks++; 
+    if(fsClicks === 1){
+        fullScreenBtn.innerHTML = "Disabled" 
+    } else if( fsClicks === 2) {
+        fullScreenBtn.innerHTML = "Enabled" 
+        fsClicks = 0; 
+    }
+}); 
 
